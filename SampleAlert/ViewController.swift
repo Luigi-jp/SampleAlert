06d3cad6c18bc8608fa06f8dece8992ad3ed2b75
luigi sato <base.basketball0607@gmail.com>
2021-07-12 23:36:00 +0900

    アクションシートを実装

--- SampleAlert/ViewController.swift
+++ SampleAlert/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnAction(_ sender: UIButton) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "アクション１", style: .default) { _ in
+            NSLog("アクション１")
+        }
+        let action2 = UIAlertAction(title: "アクション２", style: .default) { _ in
+            NSLog("アクション２")
+        }
+        let cancel = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(action1)
+        actionSheet.addAction(action2)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
 }
 
